441f1a84a8ed962dbff6ec9a31a901a4f8b9d1d1
Oscar Calle <gameplaysoscar@gmail.com>
2022-05-13 19:41:21 -0500

    Error al crear el producto, intente nuevamente

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -37,6 +37,51 @@ exports.createProduct = async (req, res) => {
       res.status(200).json(productSaved)
    } catch (error) {
       console.log(error)
-      res.status(500).json(error)
+      res.status(500).json({ message: 'Ocurrio un error con el servidor' })
+   }
+}
+
+exports.updateProduct = async (req, res) => {
+   try {
+      const isValidId = validateObjectId(req.query.productId)
+      if (!isValidId)
+         return res.status(400).json({ message: 'La ID es inválida' })
+
+      const product = await Product.findByIdAndUpdate(
+         req.query.productId,
+         {
+            $set: {
+               ...req.body,
+            },
+         },
+         { new: true }
+      )
+
+      if (!product)
+         return res.status(404).json({ message: 'El producto no existe' })
+
+      res.json(product)
+   } catch (error) {
+      console.log(error)
+      res.status(500).json({ message: 'Ocurrio un error con el servidor' })
+   }
+}
+
+exports.deleteProduct = async (req, res) => {
+   try {
+      console.log(req.query)
+      const isValidId = validateObjectId(req.query.productId)
+      if (!isValidId)
+         return res.status(400).json({ message: 'La ID es inválida' })
+
+      const product = await Product.findByIdAndDelete(req.query.productId)
+
+      if (!product)
+         return res.status(404).json({ message: 'El producto no existe' })
+
+      res.json(product)
+   } catch (error) {
+      console.log(error)
+      res.status(500).json({ message: 'Ocurrio un error con el servidor' })
    }
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,6 +3,8 @@ const {
    createProduct,
    getAllProducts,
    getProductById,
+   deleteProduct,
+   updateProduct,
 } = require('../controllers/product.controller')
 
 const router = Router()
@@ -10,5 +12,7 @@ const router = Router()
 router.get('/get-all-products', getAllProducts)
 router.get('/get-product-by-id', getProductById)
 router.post('/create-product', createProduct)
+router.put('/update-product', updateProduct)
+router.delete('/delete-product', deleteProduct)
 
 module.exports = router
